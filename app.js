9031444e669a9e7236fadb8f87da17b7245e6836
Andres Gongora Herrera <agongora@cosinte.bog>
2022-01-13 11:43:57 -0500

    Validar base de datos

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 require('dotenv').config();
 
+const { crearConexion, cerrarConexion } = require('./db/conn.js')
+
 const app = express();
 
 console.log(process.env.APP_PORT);
 
 app.get('/',function(req,res)
 {
-    console.log('Desarrollo corriendo...')
-    res.send('<h1>CIAv2 back</h1>');
+    const rescon = crearConexion();
+    const resclo = cerrarConexion();
+    res.send(`<h1>CIAv2 back</h1><br /><h2>${rescon}</h2><br /><h2>${resclo}</h2>`);
 });
 
 app.listen(process.env.APP_PORT, () =>{});
--- db/conn.js
+++ db/conn.js
@@ -1,21 +1,28 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
 
 const conn = mysql.createConnection({
-    host: "localhost",  
-    user: "root",  
-    password: ""  
+    host: process.env.DATABASE_HOST,  
+    user: process.env.DATABASE_USERNAME,  
+    password: process.env.DATABASE_PASSWORD
 });
 
-
-
 const crearConexion = () => {
-    conn.connect((err) => {  
+    const cc = conn.connect((err) => {  
         if (err) throw err;  
-        console.log("Connected!");  
-    }); 
+    });
+    return 'Se abre la conexion';
 }
 
+const cerrarConexion = () => {
+    const cc = conn.end((err) => {
+        if (err) throw err; 
+    });
+    return 'Se ha cerrado la conexion';
+}
 
-module.exports({
-    con
-})
+module.exports = {
+    crearConexion,
+    cerrarConexion
+}
